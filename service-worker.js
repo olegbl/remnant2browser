9e8ca2acd3a5b7639fe1fd1ab59bc66b33be9ebe
Oleg Lokhvitsky <olegbl@gmail.com>
2023-08-04 14:57:13 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,8 @@
-self.addEventListener('install', () => {});
-self.addEventListener('activate', () => {});
+// we don't actually need a service worker for this SPA
+// but it is required by the PWA standard
+
+self.addEventListener('install', (event) => {});
+
+self.addEventListener('activate', (event) => {});
+
+self.addEventListener('fetch', (event) => {});
