c15f5fbb2279acd10a0a28bb92ec147dda2087ae
Oleg Lokhvitsky <olegbl@gmail.com>
2023-08-04 15:02:51 -0700

    Fix PWA stuff some more

--- src/index.tsx
+++ src/index.tsx
@@ -5,3 +5,12 @@ import App from './App';
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(<App />);
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('service-worker.js')
+      .then((registration) => {})
+      .catch((error) => {});
+  });
+}
